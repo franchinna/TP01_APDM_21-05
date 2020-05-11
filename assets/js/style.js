2843277ded91c2bc865666378240e8e5194413ef
Franchinna <francinnante@gmail.com>
2020-05-11 17:52:20 -0300

    subida de cambios L&F

--- assets/js/style.js
+++ assets/js/style.js
@@ -1,25 +1,71 @@
 let btnRegistro = document.getElementById('btnRegistro');
 let btnConfirmacion = document.getElementById('btnConfirmacion');
+let btnLogin = document.getElementById('btnLogin');
+let btnLoginConfirmacion = document.getElementById('btnLoginConfirmacion');
 
 let divRegistro = document.getElementById('registro');
 let divConfirmacion = document.getElementById('confirmacion');
+let divLogin = document.getElementById('login');
+let divLoginConfirmacion = document.getElementById('loginConfirmacion');
 
 
 
 btnConfirmacion.onclick = function(){
     
     divRegistro.style.display = 'none';
+    divLogin.style.display = 'none';
+    divLoginConfirmacion.style.display = 'none';
+    
     divConfirmacion.style.display = 'block';
+    
     this.setAttribute('class', 'btn btn-warning');
     btnRegistro.setAttribute('class', 'btn btn-default');
+    btnLogin.setAttribute('class', 'btn btn-default');
+    btnLoginConfirmacion.setAttribute('class', 'btn btn-default');
     
 };
 
 btnRegistro.onclick = function(){
     
     divConfirmacion.style.display = 'none';
+    divLogin.style.display = 'none';
+    divLoginConfirmacion.style.display = 'none';
+    
     divRegistro.style.display = 'block';
+    
     btnConfirmacion.setAttribute('class', 'btn btn-default');
+    btnLogin.setAttribute('class', 'btn btn-default');
+    btnLoginConfirmacion.setAttribute('class', 'btn btn-default');
+    this.setAttribute('class', 'btn btn-warning');
+    
+};
+
+btnLogin.onclick = function(){
+    
+    divConfirmacion.style.display = 'none';
+    divRegistro.style.display = 'none';
+    divLoginConfirmacion.style.display = 'none';
+    
+    divLogin.style.display = 'block';
+    
+    btnRegistro.setAttribute('class', 'btn btn-default');
+    btnConfirmacion.setAttribute('class', 'btn btn-default');
+    btnLoginConfirmacion.setAttribute('class', 'btn btn-default');
+    this.setAttribute('class', 'btn btn-warning');
+    
+};
+
+btnLoginConfirmacion.onclick = function(){
+    
+    divConfirmacion.style.display = 'none';
+    divLogin.style.display = 'none';
+    divRegistro.style.display = 'none';
+    
+    divLoginConfirmacion.style.display = 'block';
+    
+    btnRegistro.setAttribute('class', 'btn btn-default');
+    btnConfirmacion.setAttribute('class', 'btn btn-default');
+    btnLogin.setAttribute('class', 'btn btn-default');
     this.setAttribute('class', 'btn btn-warning');
     
 };
